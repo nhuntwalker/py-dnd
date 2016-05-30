c5cb7de8c0581d30b544f9f6d704b9582087f852
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-05-30 12:13:36 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Py-DnD
+
+**Author:** Nicholas Hunt-Walker
+
+**Created:** May 30th, 2016
+
+A Python module for character creation and simple gameplay of the 5th
+edition of Dungeons and Dragons.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+extras = {"test": ["pytest", "pytest-watch", "tox"]}
+
+setup(name="pydnd",
+    description="""Py-DnD is a Python module for simple character 
+    generation for the fifth edition of Dungeons and Dragons.""",
+    version=0.1,
+    author="Nicholas Hunt-Walker",
+    author_email="nhuntwalker@gmail.com",
+    extras_require=extras
+)
